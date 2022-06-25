db11055217db0f270572be6071c61c71e81d1660
Ritvik Singh <75081330+Ritviksingh-cyber@users.noreply.github.com>
2022-06-26 00:46:46 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,11 +1,10 @@
+
 import 'package:flutter/material.dart';
-import 'package:flutter_projects/pages/loginpage.dart';
-import 'package:flutter_projects/pages/precautions.dart';
+import 'package:flutter_projects/pages/login_homepage/loginpage.dart';
 import 'package:flutter_projects/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_projects/pages/home_page.dart';
+import 'package:flutter_projects/pages/login_homepage/home_page.dart';
 
-import '../models/catalog.dart';
 void main(){
   runApp(MyApp());
 }
@@ -14,24 +13,25 @@ class MyApp extends StatelessWidget {//a stateless widget is a widget in which n
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      themeMode: ThemeMode.light,// Means the theme of our application
+      themeMode: ThemeMode.system,// Means the theme of our application
       theme: ThemeData(
           fontFamily: GoogleFonts.poppins().fontFamily,//Primary swatch is used to set the primary color of our application.
           primarySwatch: Colors.red,// we have to download the package for fonts in flutter
           primaryTextTheme: GoogleFonts.latoTextTheme()),
 
       darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        //color data can be placed here
+          fontFamily: GoogleFonts.poppins().fontFamily,//Primary swatch is used to set the primary color of our application.
+          primarySwatch: Colors.red,// we have to download the package for fonts in flutter
+          primaryTextTheme: GoogleFonts.latoTextTheme()
       ),
-      debugShowCheckedModeBanner: false,
-      initialRoute: MyRoutes.homeRoute,
+        debugShowCheckedModeBanner: false,
+      initialRoute: MyRoutes.loginRoute,
       routes:
         {
-          "/":(context)=> HomePage(),
-          MyRoutes.homeRoute:(context)=>HomePage(),//Routes to our different pages in application.
+          "/":(context)=> LoginPage(),
+          MyRoutes.homeRoute:(context)=>const HomePage(),//Routes to our different pages in application.
             MyRoutes.loginRoute:(context)=>LoginPage(),
-          MyRoutes.precautionRoute:(context)=>Precautions(),
+          MyRoutes.exerciseRoute:(context)=>HomePage(),
         }
     );
   }
